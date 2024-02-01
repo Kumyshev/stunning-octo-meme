d5657ac17a8655e80388dfdfd9ea061a5174adea
akhmed <44803512+Kumyshev@users.noreply.github.com>
2024-02-01 13:10:24 +0300

    first

---
+++ src/main/java/com/example/octomeme/OctoMemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.octomeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoMemeApplication.class, args);
+	}
+
+}
